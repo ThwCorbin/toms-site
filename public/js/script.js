6aac7837c6ecdd4e78164fdb25a52765c2187439
Thomas Corbin <30200976+ThwCorbin@users.noreply.github.com>
2019-09-10 19:31:35 -0400

    Fixed ESLINT Prettier configurations

--- public/js/script.js
+++ public/js/script.js
@@ -1,23 +1,22 @@
 //// import { render } from "nunjucks";
 let nunjucks = require("nunjucks");
 
-nunjucks.configure("views", {autoescape: true});
+// //nunjucks.configure("views", {"autoescape: true"});
 
 //// let env = nunjucks.configure("views");
 
-nunjucks.render(
-  "index.njk", {
-    page_title: "Tom's Site",
-    pub: "The Wicket Maiden",
-    drink: "Old Fashioned",
-    hours: "12:00 til 11:00"
-  }
-);
+nunjucks.render("index.njk", {
+  page_title: "Tom's Site",
+  pub: "The Wicket Maiden",
+  drink: "Old Fashioned",
+  hours: "12:00 til 11:00"
+});
 
-if(x<=12){
-  // code 
+let x = 0;
+
+if (x <= 12) {
+  // code
 }
-
 
 //# Label: Better Comments Key (added # comment style to settings.json)
 //! Warning: A Palantir is a dangerous tool, Saruman.
