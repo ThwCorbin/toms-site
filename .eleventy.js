c8b46d2b412d8c9265b7f83373b93526da732466
Thomas Corbin <30200976+ThwCorbin@users.noreply.github.com>
2019-09-10 14:34:10 -0400

    Configured 11ty, created new public views file structure, deleted obsolete dist public dir/files, added Better Comments notes

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,33 @@
-module.exports = {
-  dir: {
-    input: "views",
-    output: "dist",
-    includes: "_includes"
-  },
-  "dataTemplateEngine": "njk",
-  markdownTemplateEngine: "njk",
-  htmlTemplateEngine: "njk"
+module.exports = function(eleventyConfig) {
+
+  eleventyConfig.setTemplateFormats("html,md,njk");
+
+  return {
+    dir: {
+      input: "views", //* controls dir/file/glob where we look for templates
+      output: "dist", //* controls dir where we write finished templates
+      includes: "_includes", //* layouts, include/extend files, partials, macros
+      data: "_data" //* controls dir for global data template files
+    },
+      //# Template engines
+      //* global data files run through this before transforming to JSON
+      dataTemplateEngine: "njk",
+      //* markdown files run through this before transforming to HTML
+      markdownTemplateEngine: "njk", 
+      //* HTML templates run through this before transforming to better HTML
+      htmlTemplateEngine: "njk" 
+  };
 };
 
-
-// See 11ty docs: Using the Configuration API
+//* //Optional: See 11ty docs for using Nunjucks env...
 // module.exports = function(eleventyConfig) {
-//   let nunjucksEnvironment = new nunjucks.Environment(
-//     new nunjucks.FileSystemLoader("views")
-//   );
-
-//   eleventyConfig.setLibrary("njk", nunjucksEnvironment);
+  //* // Using the Configuration API 
+  // let nunjucksEnvironment = new nunjucks.Environment(
+  // new nunjucks.FileSystemLoader("views")
+  // );
+  //* // Use your Nunjucks Environment
+  // let nunjucksEnvironment = new Nunjucks.Environment(
+  // new Nunjucks.FileSystemLoader("_includes")
+  // );
+//  eleventyConfig.setLibrary("njk", nunjucksEnvironment);
 // };
--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,9 @@
-// import { render } from "nunjucks";
+//// import { render } from "nunjucks";
 let nunjucks = require("nunjucks");
 
 nunjucks.configure("views", {autoescape: true});
 
-// let env = nunjucks.configure("views");
+//// let env = nunjucks.configure("views");
 
 nunjucks.render(
   "index.njk", {
@@ -13,3 +13,15 @@ nunjucks.render(
     hours: "12:00 til 11:00"
   }
 );
+
+if(x<=12){
+  // code 
+}
+
+
+//# Label: Better Comments Key (added # comment style to settings.json)
+//! Warning: A Palantir is a dangerous tool, Saruman.
+//? Question: Why? Why should we fear to use it?
+//* Highlight: They are not all accounted for, the lost Seeing Stones.
+//// Strike - Friendship with Saruman is not lightly thrown aside!
+//todo: Leap off the Tower of Orthanc onto a giant eagle
