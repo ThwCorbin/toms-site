1822a0d98840dbada26bd06f9683d3e89cdd84cc
Thomas Corbin <30200976+ThwCorbin@users.noreply.github.com>
2019-09-19 18:37:07 -0400

    Configured 11ty & basic Nunjucks

--- .eleventy.js
+++ .eleventy.js
@@ -1,33 +1,29 @@
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+
 module.exports = function(eleventyConfig) {
+	eleventyConfig.setTemplateFormats('html,md,njk,css');
+	eleventyConfig.addPassthroughCopy('src/assets/images');
+	eleventyConfig.addPassthroughCopy('src/assets/js');
+	//* path is relative to root: 11ty drops "src/" when outputs to "dist/"
 
-  eleventyConfig.setTemplateFormats("html,md,njk");
+	//# 11ty syntax highlighting
+	eleventyConfig.addPlugin(syntaxHighlight);
 
-  return {
-    dir: {
-      input: "views", //* controls dir/file/glob where we look for templates
-      output: "dist", //* controls dir where we write finished templates
-      includes: "_includes", //* layouts, include/extend files, partials, macros
-      data: "_data" //* controls dir for global data template files
-    },
-      //# Template engines
-      //* global data files run through this before transforming to JSON
-      dataTemplateEngine: "njk",
-      //* markdown files run through this before transforming to HTML
-      markdownTemplateEngine: "njk", 
-      //* HTML templates run through this before transforming to better HTML
-      htmlTemplateEngine: "njk" 
-  };
+	//# Returns configuration options (Config object)
+	return {
+		dir: {
+			input: 'src', //* controls top level dir/file/glob where we look for templates
+			output: 'dist', //* controls dir where we write finished templates
+			//* Values below are relative to the input directory
+			includes: '_includes', //* layouts, include/extend files, partials, macros
+			data: '_data', //* controls dir for global data template files
+		},
+		//# Template engines
+		//* global data files run through this before transforming to JSON
+		dataTemplateEngine: 'njk',
+		//* markdown files run through this before transforming to HTML
+		markdownTemplateEngine: 'njk',
+		//* HTML templates run through this before transforming to better HTML
+		htmlTemplateEngine: 'njk',
+	};
 };
-
-//* //Optional: See 11ty docs for using Nunjucks env...
-// module.exports = function(eleventyConfig) {
-  //* // Using the Configuration API 
-  // let nunjucksEnvironment = new nunjucks.Environment(
-  // new nunjucks.FileSystemLoader("views")
-  // );
-  //* // Use your Nunjucks Environment
-  // let nunjucksEnvironment = new Nunjucks.Environment(
-  // new Nunjucks.FileSystemLoader("_includes")
-  // );
-//  eleventyConfig.setLibrary("njk", nunjucksEnvironment);
-// };
---
+++ src/assets/js/script.js
@@ -0,0 +1,6 @@
+//# Label: Better Comments Key (added # comment style to settings.json)
+//! Warning: A Palantir is a dangerous tool, Saruman.
+//? Question: Why? Why should we fear to use it?
+//* Highlight: They are not all accounted for, the lost Seeing Stones.
+//// Strike - Friendship with Saruman is not lightly thrown aside!
+//todo: Leap off the Tower of Orthanc onto a giant eagle
