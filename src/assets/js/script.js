c92c80da0bea7971ab402fc443bb2dd9d4c15273
NiceDrop <30200976+NiceDrop@users.noreply.github.com>
2019-10-02 13:16:55 -0400

    Delete test code for mobile element highlighting, added background color for mobile browsers

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -6,28 +6,13 @@
 //todo: Leap off the Tower of Orthanc onto a giant eagle
 
 let anchorCollection = document.getElementsByTagName('a');
-let outAnchors = document.querySelector('.outputAnchors');
-let hTitle = document.querySelector('.headerTitle');
-
-//# Testing why "Underline current page in nav bars" not working on iOS (possible others)
-let postAnchors = (anchorVar) => {
-	let textAnchor = document.createTextNode(anchorVar.id);
-	let liAnchor = document.createElement('li');
-	liAnchor.appendChild(textAnchor);
-	outAnchors.appendChild(liAnchor);
-};
 
 //# Underline current page in nav bars
 (() => {
 	for (let i = 0; i < anchorCollection.length; i++) {
-		let anchorVar = anchorCollection[i];
 		anchorCollection[i].attributes.href.value.toLowerCase() ==
 		window.location.pathname.toLowerCase()
-			? (anchorCollection[i].parentElement.setAttribute('id', 'currentPage'),
-			  (hTitle.style.background = 'red'),
-			  console.log(anchorCollection[i]),
-			  console.log(anchorVar),
-			  postAnchors(anchorVar))
+			? anchorCollection[i].parentElement.setAttribute('id', 'currentPage')
 			: anchorCollection[i].removeAttribute('id');
 	}
 })();
