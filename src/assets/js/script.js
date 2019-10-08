4f9194666feb23a71045aeacf752243edbad4948
Thomas Corbin <30200976+ThwCorbin@users.noreply.github.com>
2019-10-08 16:55:03 -0400

    Add script and keywords to package.json, update Better Comments key in settings

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -4,19 +4,19 @@
 // 	ignore: [],
 // });
 
-let anchorCollection = document.getElementsByTagName('a');
+let anchorCollection = document.getElementsByTagName("a");
 
-//# Underline current page in nav bars
+//> Underline current page in nav bars
 (() => {
-	for (let i = 0; i < anchorCollection.length; i++) {
-		anchorCollection[i].attributes.href.value.toLowerCase() ==
-		window.location.pathname.toLowerCase()
-			? anchorCollection[i].parentElement.setAttribute('id', 'currentPage')
-			: anchorCollection[i].removeAttribute('id');
-	}
+  for (let i = 0; i < anchorCollection.length; i++) {
+    anchorCollection[i].attributes.href.value.toLowerCase() ==
+    window.location.pathname.toLowerCase()
+      ? anchorCollection[i].parentElement.setAttribute("id", "currentPage")
+      : anchorCollection[i].removeAttribute("id");
+  }
 })();
 
-//# Label: Better Comments Key (added # comment style to settings.json)
+//> Label: Better Comments Key (added # comment style to settings.json)
 //! Warning: A Palantir is a dangerous tool, Saruman.
 //? Question: Why? Why should we fear to use it?
 //* Highlight: They are not all accounted for, the lost Seeing Stones.
