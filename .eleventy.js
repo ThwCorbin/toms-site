4bb22f7f9e9ceb99de7abeee0df70474cacf68f3
NiceDrop <30200976+NiceDrop@users.noreply.github.com>
2019-10-22 08:50:39 -0400

    Change addPassthroughCopy to src/assets/build

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,7 @@ const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 module.exports = function(eleventyConfig) {
   eleventyConfig.setTemplateFormats('html,md,njk,css');
   eleventyConfig.addPassthroughCopy('src/assets/images');
-  eleventyConfig.addPassthroughCopy('src/assets/bundle');
+  eleventyConfig.addPassthroughCopy('src/assets/build');
   //* path is relative to root: 11ty drops "src/" when outputs to "dist/"
 
   //> Parse exerpts from content
