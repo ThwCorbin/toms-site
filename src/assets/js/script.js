e6ec95d8ed3cf63575e92112f94f8401ee4a03ab
NiceDrop <30200976+NiceDrop@users.noreply.github.com>
2019-10-22 21:21:23 -0400

    Update import/export and click tests

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -25,7 +25,7 @@ let anchorCollection = document.getElementsByTagName("a");
   }
 })();
 
-//> Label: Better Comments Key (added # comment style to settings.json)
+//> Label: Better Comments Key (added > comment style to settings.json)
 //! Warning: A Palantir is a dangerous tool, Saruman.
 //? Question: Why? Why should we fear to use it?
 //* Highlight: They are not all accounted for, the lost Seeing Stones.
